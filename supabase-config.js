e4ae5da3724775cb466c0275c23204b0ff0ca8ec
Alesandro Carvalho <carvalho.alesandro@gmail.com>
2026-03-19 22:13:41 -0300

    feat: add supabase talp session flow

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+window.PROFEF_SUPABASE_CONFIG = {
+  url: "https://uqpqgjldxoqzqgovkxjw.supabase.co",
+  anonKey: "sb_publishable_o7B-Ybbpr2DnE3jqqJd3nQ_etzDPCxS",
+  alunoPagePath: "./talp-aluno.html"
+};
+
